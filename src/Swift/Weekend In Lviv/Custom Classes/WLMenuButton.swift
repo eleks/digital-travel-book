efd7f9af566bbf10d07200aedcab8291da6b379d
YuriyZhuravel <yura.zhuravel.ios@gmail.com>
2014-06-13 17:03:01 +0300

    Added Swift project to workspace
    - initial commit for swift
    - added classes/sources to project
    - included MMDrawerController and UIImage+Additions components

---
+++ src/Swift/Weekend In Lviv/Custom Classes/WLMenuButton.swift
@@ -0,0 +1,13 @@
+//
+//  WLMenuButton.swift
+//  Weekend In Lviv
+//
+//  Created by Admin on 13.06.14.
+//  Copyright (c) 2014 rnd. All rights reserved.
+//
+
+import UIKit
+
+class WLMenuButton: UIBarButtonItem {
+   
+}
---
+++ src/Swift/Weekend In Lviv/Libs-obj-с/Weekend_In_Lviv-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Weekend In Lviv-Bridging-Header.h
+//  Weekend In Lviv
+//
+//  Created by Admin on 13.06.14.
+//  Copyright (c) 2014 rnd. All rights reserved.
+//
+
+#import "MMDrawerBarButtonItem.h"
+#import "MMDrawerController.h"
+#import "MMDrawerController+Subclass.h"
+#import "MMDrawerVisualState.h"
+#import "UIViewController+MMDrawerController.h"
+
+#import "UIImage+Additions.h"
